076d474ac4c12d351c6580e233561aec0bce82a5
jsvisa <delweng@gmail.com>
2013-07-22 21:46:38 +0800

    Added email valiable authentication

--- jsvisa/chapter2_demo/app/models/user.rb
+++ jsvisa/chapter2_demo/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+	validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+	#validates :email, :format => do
+	#	:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create 
+	#end
 end
