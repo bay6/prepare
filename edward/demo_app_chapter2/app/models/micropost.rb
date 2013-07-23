b440746db8a31eade7bdd464661d3534d276bdce
Edward Chan <chankaward@gmail.com>
2013-07-23 13:58:04 +0800

    fixed

--- edward/demo_app_chapter2/app/models/micropost.rb
+++ edward/demo_app_chapter2/app/models/micropost.rb
@@ -1,5 +1,4 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :content, length: { maximum: 140 }
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
