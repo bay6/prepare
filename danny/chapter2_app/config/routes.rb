a6693c53dcc95bc80def20b20064167cda40fb39
dany <idanyzheng@gmail.com>
2013-07-23 10:38:06 +0800

    chapter2_app done

--- danny/chapter2_app/config/routes.rb
+++ danny/chapter2_app/config/routes.rb
@@ -1,4 +1,8 @@
 Chapter2App::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
