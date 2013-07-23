865bb4afcf4ed9f30a45358b797f9a06cef6afb8
Edward Chan <chankaward@gmail.com>
2013-07-23 13:54:19 +0800

    add validation of valid email

--- edward/demo_app_chapter2/app/models/micropost.rb
+++ edward/demo_app_chapter2/app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :content, length: { maximum: 140 }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
