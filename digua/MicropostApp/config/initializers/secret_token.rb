d32b60e413dd933c760b91ea671611b33d32a078
diguage <leejun119@gmail.com>
2013-07-23 03:01:17 +0000

    Init Micropost Project

---
+++ digua/MicropostApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MicropostApp::Application.config.secret_key_base = 'b2bb77c8b6a838bfba3ce12230ec6f6cfd2e83b31c541dda470206a918f7747536d744f5b79e013ee91c4ae20fc6c7e5f7ebf7eb6d2365e7bfe701b5ec4a9cb5'
