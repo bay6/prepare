fbd9f9c82d5cb8693cc2cc1bac92d562b59dc9ed
dany <idanyzheng@gmail.com>
2013-07-24 22:04:00 +0800

    Improve rspec

--- danny/microblog_app/spec/requests/static_pages_spec.rb
+++ danny/microblog_app/spec/requests/static_pages_spec.rb
@@ -1,38 +1,44 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  # subject { page }
   describe "Home page" do
+    before { visit root_path }
     it "should have the content 'Microblog App' " do
-      visit '/static_pages/home'
       expect(page).to have_content('Microblog App')
     end
+    # it {should have_content('Microblog App')}
 
     it "should have the title 'Home' " do
-      visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title(full_title("Home"))
     end
 
   end
   describe "Help page" do
+    before { visit help_path }
     it "should have the content 'Help' " do
-      visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
     it "should have the title 'Help' " do
-      visit '/static_pages/help'
-      expect(page).to have_title("Help")
+      expect(page).to have_title(full_title "Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
     it "should have the content 'About' " do
-      visit '/static_pages/about'
       expect(page).to have_content('About')
     end
     it "should have the title 'About Us' " do
-      visit '/static_pages/about'
-      expect(page).to have_title("About Us")
+      expect(page).to have_title(full_title "About Us")
     end
+  end
+
+  describe "Contact Page" do
+    before { visit contact_path }
+  	it "should have the content 'Contact' " do
+	  	expect(page).to have_title(full_title "Contact")
+  	end
   end
 
 end
