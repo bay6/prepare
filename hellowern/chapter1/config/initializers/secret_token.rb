38e20a8e61b6bf68011b20a65451b3353008ae47
Hellowern <yanshunfei516@gmail.com>
2013-07-25 01:37:25 +0800

    add chpater1

---
+++ hellowern/chapter1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chapter1::Application.config.secret_key_base = '4169cc25d5ed4a2aee364fb0dc9a7786820fceb6d5a0dc1e2a06e4d103d0a0da674fd35a5104121ec92e3f2c2a939241ddf0f68fd32c35deac39b1e369923ce4'
