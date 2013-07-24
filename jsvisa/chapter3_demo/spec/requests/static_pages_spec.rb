752ad77fa9c42f8c5a8d9d8455e41861bc878a45
jsvisa <delweng@gmail.com>
2013-07-24 08:11:14 +0800

    Learning spec test

--- jsvisa/chapter3_demo/spec/requests/static_pages_spec.rb
+++ jsvisa/chapter3_demo/spec/requests/static_pages_spec.rb
@@ -13,9 +13,31 @@ require 'spec_helper'
 describe "Static pages" do
 
 	describe "Home page" do
-	    it "should have the content 'Sample App'" do
+
+	    it "should have the right content and title 'Sample App'" do
 		    visit '/static_pages/home'
 	        expect(page).to have_content('Sample App')
+	        expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
 	    end
 	end
+
+	describe "Help page" do
+
+	    it "should have the content 'Help' and the title 'Help'" do
+		    visit '/static_pages/help'
+	        expect(page).to have_content('Help')
+	        expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+	    end
+	end
+
+	describe "About page" do
+
+	    it "should have the content 'About us' and the title 'About'" do
+		    visit '/static_pages/about'
+	        expect(page).to have_content('About us')
+	        expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+	    end
+	end
+
 end
+
