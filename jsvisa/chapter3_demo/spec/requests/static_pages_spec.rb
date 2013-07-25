5cfcf463ea3e92785cf7dfc389f98890edad1b0f
jsvisa <delweng@gmail.com>
2013-07-25 20:26:43 +0800

    Guard test

--- jsvisa/chapter3_demo/spec/requests/static_pages_spec.rb
+++ jsvisa/chapter3_demo/spec/requests/static_pages_spec.rb
@@ -12,12 +12,14 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title){"Chapter3Demo"}
+
 	describe "Home page" do
 
 	    it "should have the right content and title 'Sample App'" do
 		    visit '/static_pages/home'
 	        expect(page).to have_content('Sample App')
-	        expect(page).to have_title('Chapter3Demo | Home')
+	        expect(page).to have_title("#{base_title} | Home")
 	    end
 	end
 
@@ -26,7 +28,7 @@ describe "Static pages" do
 	    it "should have the content 'Help' and the title 'Help'" do
 		    visit '/static_pages/help'
 	        expect(page).to have_content('Help')
-	        expect(page).to have_title('Chapter3Demo | Help')
+	        expect(page).to have_title("#{base_title} | Help")
 	    end
 	end
 
@@ -35,7 +37,7 @@ describe "Static pages" do
 	    it "should have the content 'About us' and the title 'About'" do
 		    visit '/static_pages/about'
 	        expect(page).to have_content('About us')
-	        expect(page).to have_title('Chapter3Demo | About')
+	        expect(page).to have_title("#{base_title} | About")
 	    end
 	end
 
@@ -44,7 +46,7 @@ describe "Static pages" do
 	    it "should have the content 'Contact' and the title 'Contact'" do
 		    visit '/static_pages/contact'
 	        expect(page).to have_content('Contact')
-	        expect(page).to have_title('Chapter3Demo | Contact')
+	        expect(page).to have_title("#{base_title} | Contact")
 	    end
 	end
 
