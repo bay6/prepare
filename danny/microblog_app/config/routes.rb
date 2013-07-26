45c80fef27d8e7a65dcc0ab6e3158e8cac8a71f1
dany <idanyzheng@gmail.com>
2013-07-26 23:06:21 +0800

    added debuginfor,FactoryGirl,Gravatar

--- danny/microblog_app/config/routes.rb
+++ danny/microblog_app/config/routes.rb
@@ -1,5 +1,6 @@
 MicroblogApp::Application.routes.draw do
-  # get "user/new"
+
+  # get "users/new"
   # get "static_pages/home"
   # get "static_pages/help"
   # get "static_pages/about"
@@ -8,7 +9,8 @@ MicroblogApp::Application.routes.draw do
   match '/help',    to: "static_pages#help",    via:'get'
   match '/about',   to: "static_pages#about",   via:'get'
   match '/contact', to: "static_pages#contact", via:'get'
-  match '/signup',  to: "user#new", via:'get'
+  match '/signup',  to: "users#new", via:'get'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- danny/microblog_app/spec/requests/user_pages_spec.rb
+++ danny/microblog_app/spec/requests/user_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "UserPages" do
+	# subject{page}
+
   describe "signup page" do
 		before { visit signup_path }
     it "have the content 'Signup' " do
@@ -10,4 +12,19 @@ describe "UserPages" do
     	expect(page).to have_title(full_title "Signup")
     end
   end
+  describe "profile page" do
+  	let(:user) {FactoryGirl.create(:user)}
+		before { visit user_path(user) }
+    # it { should have_content(user.name)}
+    # it { should have_title(full_title user.name)}
+    it "got right content  " do
+    	expect(page).to have_content(user.name)
+    end
+    it "got right title " do
+    	expect(page).to have_title(full_title user.name)
+    end
+  end
+
+
+
 end
