2dc91f6c0dd48509a19c7dde1a410ef1f18120e5
xiongbo <xiongbo027@gmail.com>
2013-07-28 15:24:51 +0800

    Finish static pages

--- xiongbo/sample_app/spec/requests/static_pages_spec.rb
+++ xiongbo/sample_app/spec/requests/static_pages_spec.rb
@@ -6,14 +6,36 @@ describe "Static Pages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', text: 'Sample App')
+    end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', text: 'Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_title("Ruby on Rails Tutorial Sample App | Help")
+    end 
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', text: 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end 
   end
 end
