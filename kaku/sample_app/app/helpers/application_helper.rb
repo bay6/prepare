68355af0f198edcf072dd32f0d07d6664789f9f3
kaku87 <ghjcumt2008@gmail.com>
2013-07-30 00:42:39 +0900

    add chapter 5

--- kaku/sample_app/app/helpers/application_helper.rb
+++ kaku/sample_app/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorail Sample App"
+    base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
     else
--- kaku/sample_app/config/routes.rb
+++ kaku/sample_app/config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
   root to: 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
