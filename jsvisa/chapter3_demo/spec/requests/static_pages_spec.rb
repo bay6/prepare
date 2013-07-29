8cb4c72137185f7172933edc1956b84b501f6ad1
jsvisa <delweng@gmail.com>
2013-07-30 00:25:33 +0800

    Complete rspec links

--- jsvisa/chapter3_demo/spec/requests/static_pages_spec.rb
+++ jsvisa/chapter3_demo/spec/requests/static_pages_spec.rb
@@ -14,11 +14,17 @@ describe "Static pages" do
   #let(:base_title) { "Chapter3Demo" }
 
   subject { page }
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
 	describe "Home page" do
     before { visit root_path }
-    it { should have_content('Sample App') }
-    it { should have_title("") }
+    let(:heading) { "Sample App" }
+    let(:page_title) { "" }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
 
 	  #it "should have the right content and title 'Sample App'" do
@@ -31,8 +37,10 @@ describe "Static pages" do
 
 	describe "Help page" do
     before { visit help_path }
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading) { "Help" }
+    let(:page_title) { "Help" }
+
+    it_should_behave_like "all static pages"
 
 	  #it "should have the content 'Help' and the title 'Help'" do
 		#  visit help_path
@@ -43,8 +51,10 @@ describe "Static pages" do
 
 	describe "About page" do
     before { visit about_path }
-    it { should have_content('About us') }
-    it { should have_title(full_title('About')) }
+    let(:heading) { "About us" }
+    let(:page_title) { "About" }
+
+    it_should_behave_like "all static pages"
 
 	  #it "should have the content 'About us' and the title 'About'" do
 		#  visit about_path
@@ -55,8 +65,10 @@ describe "Static pages" do
 
 	describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:heading) { "Contact" }
+    let(:page_title) { "Contact" }
+
+    it_should_behave_like "all static pages"
 
 	  #it "should have the content 'Contact' and the title 'Contact'" do
 	  #  visit contact_path
@@ -65,5 +77,24 @@ describe "Static pages" do
 	  #end
 	end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title("About")) 
+#    it { should have_title(full_title("About us")) }
+    click_link "Help"
+    expect(page).to have_title(full_title("Help")) 
+    click_link "Contact"
+    expect(page).to have_title(full_title("Contact")) 
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title("Sign Up")) 
+  end
+ 
+#  describe "Right links" do
+#  before { visit root_path }
+#  click_link "About"
+#  it { should have_title(full_title("About")) }
 end
+
 
