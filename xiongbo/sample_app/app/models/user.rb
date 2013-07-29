9d8b8e901c7fa1b0170938de1aa3a32fdab112eb
xiongbo <xiongbo027@gmail.com>
2013-07-29 17:06:28 +0800

    add user model

---
+++ xiongbo/sample_app/app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
+end
