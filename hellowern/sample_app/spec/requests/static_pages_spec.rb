4024744a58f03eb0d578fda369b7ed8aff415861
Hellowern <yanshunfei516@gmail.com>
2013-07-30 00:16:10 +0800

    add title exercise

--- hellowern/sample_app/spec/requests/static_pages_spec.rb
+++ hellowern/sample_app/spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static Pages" do
 		 visit '/static_pages/home'
 		 expect(page).to have_content('Sample App')
     end
+
+     it "should have the right title" do
+         visit '/static_pages/home'
+         expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
 	describe "Help Page" do
@@ -16,6 +21,11 @@ describe "Static Pages" do
 	     visit '/static_pages/help'
 	     expect(page).to have_content('Help')
 	end
+     
+     it "should have the right title" do
+         visit '/static_pages/help'
+         expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
     describe "About Page" do
@@ -23,6 +33,11 @@ describe "Static Pages" do
       it "should have the content 'About Us'" do
          visit '/static_pages/about'
          expect(page).to have_content('About Us')
+    end
+
+      it "should have the right title" do
+         visit '/static_pages/about'
+         expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
