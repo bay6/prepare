b2e7e471872e7ae3229359741c97d5d65d40a4b0
xiongbo <xiongbo027@gmail.com>
2013-07-30 21:14:47 +0800

    add sign-up

--- xiongbo/sample_app/app/controllers/users_controller.rb
+++ xiongbo/sample_app/app/controllers/users_controller.rb
@@ -9,6 +9,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+  
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   private
