097a6874a58485352d840c2ed67e2c723dcce85e
xiongbo <xiongbo027@gmail.com>
2013-07-30 17:58:02 +0800

    add user test for factory_girl

--- xiongbo/sample_app/app/controllers/users_controller.rb
+++ xiongbo/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   end
 
--- xiongbo/sample_app/spec/requests/user_pages_spec.rb
+++ xiongbo/sample_app/spec/requests/user_pages_spec.rb
@@ -9,4 +9,14 @@ describe "UserPages" do
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
+
+  describe "profile page" do
+    # let(:user) { User.create(name: "xiongbo", email: "reg027@qq.com",
+                              #password: "foobar", password_confirmation: "foobar") }
+    let(:user) { User.first }
+    before { visit user_url(user) }
+
+    it { should have_title(user.name) }
+    it { should have_selector('h1', text: user.name) }
+  end
 end
