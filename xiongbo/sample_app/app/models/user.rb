0a88fdb67441558db2e120c617fe5055330171d1
xiongbo <xiongbo027@gmail.com>
2013-07-30 08:54:00 +0800

    add email password validate and test

--- xiongbo/sample_app/app/models/user.rb
+++ xiongbo/sample_app/app/models/user.rb
@@ -12,4 +12,16 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, 
+    presence: true, 
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+
+  #validates :password, presence: true, length: { minimum: 6 }
+  #validates :password_confirmation, presence: true
 end
--- xiongbo/sample_app/spec/models/user_spec.rb
+++ xiongbo/sample_app/spec/models/user_spec.rb
@@ -12,16 +12,28 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(name: "Example User", 
+                            email: "user@example.com"
+                            password: "foobar",
+                            password_confirmation: "foobar"
+                           ) }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should be_valid }
 
   describe "when name is not present" do
     before { @user.name = "" }
+    it { should_not be_valid }
+  end 
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
@@ -30,8 +42,48 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.com user_at_foo@123.com]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
+
+  describe "when email addresses is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save 
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = "" }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password_confirmation is nil" do
+    before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
 end
