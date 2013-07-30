6c9af787e035dc85496706e98f0e3232ec6e383d
Ken Lu <ken@seeice.com>
2013-07-30 08:03:46 -0400

    some typo of migration #49

--- danny/microblog_app/db/schema.rb
+++ danny/microblog_app/db/schema.rb
@@ -23,6 +23,6 @@ ActiveRecord::Schema.define(version: 20130727135343) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_rember_token"
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
