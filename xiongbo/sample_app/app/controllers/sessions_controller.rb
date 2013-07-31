001a6c16ed1cfefe570aba311d2a2afc93aa0dfe
xiongbo <xiongbo027@gmail.com>
2013-07-31 12:51:38 +0800

    add signin view

--- xiongbo/sample_app/app/controllers/sessions_controller.rb
+++ xiongbo/sample_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+  
+  end
 end
--- xiongbo/sample_app/spec/requests/authentication_pages_spec.rb
+++ xiongbo/sample_app/spec/requests/authentication_pages_spec.rb
@@ -6,7 +6,7 @@ describe "AuthenticationPages" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selecotr('h1', text: 'Sign in') }
+    it { should have_selector('h1', text: 'Sign in') }
     it { should have_title('Sign in') }
   end
 end
