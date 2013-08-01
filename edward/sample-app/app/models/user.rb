9e08ca7f3fccbfaf878df0e675a7623fa48a08aa
Edward Chan <chankaward@gmail.com>
2013-08-02 01:46:25 +0800

    Finish user signup

--- edward/sample-app/app/models/user.rb
+++ edward/sample-app/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
--- edward/sample-app/spec/models/user_spec.rb
+++ edward/sample-app/spec/models/user_spec.rb
@@ -98,4 +98,14 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.coM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 end
