54301625eef462b0eefdaf147823d34790658a60
xiongbo <xiongbo027@gmail.com>
2013-08-02 21:03:01 +0800

    add delete micropost

--- xiongbo/sample_app/app/controllers/microposts_controller.rb
+++ xiongbo/sample_app/app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.new(post_params)
@@ -12,11 +13,17 @@ class MicropostsController < ApplicationController
     end 
   end
 
-  def destroy
-
+  def destroy 
+    @micropost.destroy
+    redirect_to root_url
   end
 
   private
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
   
   def post_params
     params.require(:micropost).permit(:content)
