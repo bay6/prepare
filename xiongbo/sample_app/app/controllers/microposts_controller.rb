4f2b11cbb1d29c4e173e374f973f7720e23991a9
xiongbo <xiongbo027@gmail.com>
2013-08-02 20:18:21 +0800

    add micropost form and modify error_message

--- xiongbo/sample_app/app/controllers/microposts_controller.rb
+++ xiongbo/sample_app/app/controllers/microposts_controller.rb
@@ -2,10 +2,22 @@ class MicropostsController < ApplicationController
   before_filter :signed_in_user
 
   def create
-
+    @micropost = current_user.microposts.new(post_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end 
   end
 
   def destroy
 
+  end
+
+  private
+  
+  def post_params
+    params.require(:micropost).permit(:content)
   end
 end
