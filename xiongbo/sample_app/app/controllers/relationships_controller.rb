7752d8971abc9bff3783fa6870758ab33aa9a7ec
xiongbo <xiongbo027@gmail.com>
2013-08-04 07:29:31 +0800

    add ajax

--- xiongbo/sample_app/app/controllers/relationships_controller.rb
+++ xiongbo/sample_app/app/controllers/relationships_controller.rb
@@ -4,12 +4,18 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js 
+    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js 
+    end
   end
 end
