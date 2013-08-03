585a32535702ce0d2bf97238c7da39db3cd4ccf9
xiongbo <xiongbo027@gmail.com>
2013-08-03 10:31:55 +0800

    add relationship test

--- xiongbo/sample_app/spec/models/relationship_spec.rb
+++ xiongbo/sample_app/spec/models/relationship_spec.rb
@@ -22,4 +22,14 @@ describe Relationship do
     its(:follower) { should == follower }
     its(:followed) { should == followed }
   end
+
+  describe "when followed_id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower_id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
 end
