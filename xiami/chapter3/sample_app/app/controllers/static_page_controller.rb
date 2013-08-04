c520bc1e1002f6ea4e681f60a8d9147bec19339e
xiamihappy <xiaonan1000@gmail.com>
2013-08-04 18:15:35 +0800

    add a staticpage controller

---
+++ xiami/chapter3/sample_app/app/controllers/static_page_controller.rb
@@ -0,0 +1,7 @@
+class StaticPageController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+end
--- xiami/chapter3/sample_app/config/routes.rb
+++ xiami/chapter3/sample_app/config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "static_page/home"
+  get "static_page/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
