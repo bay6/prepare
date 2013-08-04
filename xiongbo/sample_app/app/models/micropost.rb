b9cf783b439b98ee508381dec479a1d749dfb827
xiongbo <xiongbo027@gmail.com>
2013-08-04 09:22:11 +0800

    finish user following

--- xiongbo/sample_app/app/models/micropost.rb
+++ xiongbo/sample_app/app/models/micropost.rb
@@ -18,7 +18,7 @@ class Micropost < ActiveRecord::Base
   default_scope { order 'created_at DESC' }
 
   def self.from_user_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user)
   end
 end
