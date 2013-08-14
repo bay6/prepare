9965b08608befe5059f4f18832ad8470a9b680b9
Edward Chan <chankaward@gmail.com>
2013-08-14 12:44:03 +0800

    Add user microposts

--- edward/sample-app/app/models/user.rb
+++ edward/sample-app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   before_create :create_remember_token
 
@@ -15,6 +16,10 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
--- edward/sample-app/spec/requests/static_pages_spec.rb
+++ edward/sample-app/spec/requests/static_pages_spec.rb
@@ -32,6 +32,22 @@ describe "Static Pages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_title('| home') }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   describe "Help page" do
