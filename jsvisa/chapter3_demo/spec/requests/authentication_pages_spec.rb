5ad7c656fed7d903bc25439e233c206786911ff2
jsvisa <delweng@gmail.com>
2013-08-18 23:03:28 +0800

    display following and followers count

--- jsvisa/chapter3_demo/spec/requests/authentication_pages_spec.rb
+++ jsvisa/chapter3_demo/spec/requests/authentication_pages_spec.rb
@@ -73,6 +73,18 @@ describe "AuthenticationPages" do
             it { should have_title("Edit user") }
           end
 
+          describe "visiting the following page" do
+            before { visit following_user_path(user) }
+
+            it { should have_title("Sign in") }
+          end
+
+          describe "visiting the followers page" do
+            before { visit followers_user_path(user) }
+
+            it { should have_title("Sign in") }
+          end
+
           describe "when signing in again" do
             before do
               delete signout_path
